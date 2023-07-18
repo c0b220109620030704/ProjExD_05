4c59902bd6bc8ccec3f2800e3fa35f1996c083d6
Sunager <c0b220829c@deu.teu.ac.jp>
2023-07-18 16:17:29 +0900

    実装完了

--- heborisu.py
+++ heborisu.py
@@ -1,12 +1,16 @@
 import pygame
 import random
+import time
+
 
 # ゲーム画面の設定
-WIDTH, HEIGHT = 400, 650
+pygame.init()
+WIDTH, HEIGHT = 700, 650
 GRID_SIZE = 30
-GRID_WIDTH, GRID_HEIGHT = WIDTH // GRID_SIZE, HEIGHT // GRID_SIZE
+GRID_WIDTH, GRID_HEIGHT = 600 // GRID_SIZE, HEIGHT // GRID_SIZE
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+rec=0
 
 # テトリスのブロックの定義
 tetriminos = [
@@ -18,6 +22,7 @@ tetriminos = [
     [[1, 1, 0], [0, 1, 1]],
     [[0, 1, 0], [1, 1, 1]]
 ]
+
 
 # テトリスのブロックを表すクラス
 class Tetrimino:
@@ -33,7 +38,6 @@ class Tetrimino:
     def move(self, dx, dy):
         self.x += dx
         self.y += dy
-
     def draw(self):
         for y, row in enumerate(self.shape):
             for x, col in enumerate(row):
@@ -49,12 +53,24 @@ class Tetrimino:
                             grid[self.y + y][self.x + x]):
                     return True
         return False
-
     def place(self, grid):
         for y, row in enumerate(self.shape):
             for x, col in enumerate(row):
                 if col:
                     grid[self.y + y][self.x + x] = self.color
+
+#def time_str(val):#数字の文字列を生成
+ #   sec=int(val)
+  #  ms=int((val-sec)*90)
+   # return "{}{:02}".format(sec%60,ms)
+
+def draw_text(screen, txt, x, y, col, fnt):
+    sur = fnt.render(txt, True, (255,255,255))#白色で文字列をかいたサーフェスを生成
+    x -= sur.get_width()/2
+    y -= sur.get_height()/2
+    screen.blit(sur, [x+2, y+2])
+    #sur = fnt.render(txt, True, col)
+    #screen.blit(sur, [x, y])
 
 # ゲームの初期化
 def init_game():
@@ -63,10 +79,18 @@ def init_game():
     game_over = False
     return grid, tetrimino, game_over
 
+#def show(self):
+ #       font = pygame.font.Font(None, 50)
+  #      text1 = font.render("経過時間:", True, (255, 0, 0))
+   #     level = font.render("{clock}".format(self.level), True, (255, 255, 255))
+    #    screen.blit(text1, [200, 300])
+     #   screen.blit(level, [700, 300])
+
 # ゲームのメインループ
 def run_game():
+    global rec
+    fnt_s = pygame.font.Font(None,  40)
     grid, tetrimino, game_over = init_game()
-
     while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -94,10 +118,12 @@ def run_game():
             for x, col in enumerate(row):
                 if col:
                     pygame.draw.rect(screen, col, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
-
+        rec=rec+0.01/2
+        draw_text(screen, "time "+time_str(rec), 630, 80, (255,255,255), fnt_s)
         tetrimino.draw()
         pygame.display.flip()
         clock.tick(3)
+        print(time_str(rec))
 
 # ゲームの実行
 run_game()
